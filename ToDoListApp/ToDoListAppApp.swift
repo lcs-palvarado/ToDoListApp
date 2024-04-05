f2dda45d7b6c59456369d6408a0b205f977fab86
Pablo Alvarado <palvarado@lcs.on.ca>
2024-04-05 09:42:23 -0400

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Pablo Alvarado on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
